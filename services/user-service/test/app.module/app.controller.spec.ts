1c556b55d233b611f02f1dfc97bc50f5eada6042
wmhindika <wmhindika@gmail.com>
2022-01-19 17:54:41 +0530

    update health check message

--- services/user-service/test/app.module/app.controller.spec.ts
+++ services/user-service/test/app.module/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World! Indika');
+      expect(appController.healthCheck()).toBe({ status: 'OK', message: 'Running' });
     });
   });
 });
